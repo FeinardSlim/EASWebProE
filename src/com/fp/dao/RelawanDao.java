0ac0041a448aa941c0aea1a9efb48ac29bf1ba6f
aulkiller <58225261+aulkiller@users.noreply.github.com>
2020-11-28 18:05:47 +0700

    postrekkkkks sql

--- src/com/fp/dao/RelawanDao.java
+++ src/com/fp/dao/RelawanDao.java
@@ -11,8 +11,8 @@ public class RelawanDao
 	{  
 	    Connection con=null;  
 	    try{  
-	        Class.forName("com.mysql.jdbc.Driver");  
-	        con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");  
+	    	Class.forName("org.postgresql.Driver");  
+	        con=DriverManager.getConnection("jdbc:postgresql://ec2-54-228-209-117.eu-west-1.compute.amazonaws.com:5432/d22sbhc83lf1bm","wjujmsmbpkjtcp","9ecffbbb1e3e391206b21aef8bf5f9c91fbe669de48472da69b7b55a63308fe8");  
 	    }catch(Exception e){System.out.println(e);}  
 	    return con;  
 	}  
